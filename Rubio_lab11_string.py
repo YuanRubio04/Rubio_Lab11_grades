776ce055babc9f6f2de25e4c5e77a1de4f7a2b59
YuanRubio04 <yuan.aundre@gmail.com>
2024-11-13 14:42:19 +0800

    Update Rubio_lab11_string.py

--- Rubio_lab11_string.py
+++ Rubio_lab11_string.py
@@ -1,17 +1,13 @@
-count = 0
 listofwords = []
 matching_words = []
 
-for i in range(10):
-        while True:
-            word = input("Enter a word: ")
-        listofwords.append(word)
-        count+=1
-
+while count<10:
+    word = input("Enter 10 set of words: ")
+    listofwords.append(word)
+    count+=1
+        
 number=int(input("Enter a number: "))
 for i in listofwords:
     if len(i)>=number:
         matching_words.append(i)
 print(matching_words)
-        
-    
